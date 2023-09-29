8479c8401ea44be4196dbdc0a6f75e8ba1acff90
vsinghmi123 <62672251+vsinghmi123@users.noreply.github.com>
2023-09-29 10:22:20 +0530

    Update api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static String api = "Your API Key";
+  static String api = "f16de0ec-a709-4b99-b54c-bd20d4646e09";
 
   static String currentMatches =
       "https://api.cricapi.com/v1/currentMatches?apikey=$api&offset=0";
